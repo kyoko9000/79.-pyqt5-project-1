7bde76043f16dbdf755f655c720a10ea82069491
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-03-31 16:15:39 +0700

    update

---
+++ 9.qthread_control.py
@@ -0,0 +1,88 @@
+import sys
+import time
+
+import cv2
+import numpy as np
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+from gui3 import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):  # Show GUI
+        super().__init__()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self)
+
+        self.label = QtWidgets.QLabel(self.uic.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(80)
+        self.label.setFont(font)
+        self.label.setText("this is a label")
+        self.uic.verticalLayout.addWidget(self.label)
+
+        self.Button = QtWidgets.QPushButton(self.uic.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(40)
+        self.Button.setFont(font)
+        self.Button.setText("update")
+        self.uic.verticalLayout.addWidget(self.Button)
+
+        self.Button1 = QtWidgets.QPushButton(self.uic.centralwidget)
+        font = QtGui.QFont()
+        font.setPointSize(40)
+        self.Button1.setFont(font)
+        self.Button1.setText("stop")
+        self.uic.verticalLayout.addWidget(self.Button1)
+
+        self.uic.pushButton.setText("start")
+
+        self.uic.pushButton.clicked.connect(self.start_thread)
+        self.Button1.clicked.connect(self.stop_thread)
+        self.thread = {}
+
+    def start_thread(self):
+        self.thread[1] = thread_section(index=1)
+        self.thread[1].start()
+        self.thread[1].signal.connect(self.show_data)
+
+    def stop_thread(self):
+        self.thread[1].stop_app()
+
+    def show_data(self, data):
+        self.label.setText(str(data))
+
+
+class thread_section(QThread):
+    signal = pyqtSignal(object)
+
+    def __init__(self, index):
+        self.stop = False
+        self.index = index
+        print("Starting threading: ", self.index)
+        super().__init__()
+
+    def run(self):
+        i = 0
+        while True:
+            i += 1
+            time.sleep(1)
+            print(i)
+            self.signal.emit(i)
+            if self.stop:
+                break
+
+    def stop_app(self):
+        print("stop")
+        self.stop = True
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
